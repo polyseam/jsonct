ca1e0587a4c8160c01aa9fdfb2987a4f17dffa1e
Matt Johnston <m@ttjohnston.com>
2024-05-14 21:40:43 -0300

    decent colors

--- code-extension/out/extension.js
+++ code-extension/out/extension.js
@@ -1,66 +1,45 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) ||
-  (Object.create
-    ? (function (o, m, k, k2) {
-      if (k2 === undefined) k2 = k;
-      var desc = Object.getOwnPropertyDescriptor(m, k);
-      if (
-        !desc ||
-        ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)
-      ) {
-        desc = {
-          enumerable: true,
-          get: function () {
-            return m[k];
-          },
-        };
-      }
-      Object.defineProperty(o, k2, desc);
-    })
-    : (function (o, m, k, k2) {
-      if (k2 === undefined) k2 = k;
-      o[k2] = m[k];
-    }));
-var __setModuleDefault = (this && this.__setModuleDefault) ||
-  (Object.create
-    ? (function (o, v) {
-      Object.defineProperty(o, "default", { enumerable: true, value: v });
-    })
-    : function (o, v) {
-      o["default"] = v;
-    });
-var __importStar = (this && this.__importStar) || function (mod) {
-  if (mod && mod.__esModule) return mod;
-  var result = {};
-  if (mod != null) {
-    for (var k in mod) {
-      if (
-        k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)
-      ) __createBinding(result, mod, k);
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
     }
-  }
-  __setModuleDefault(result, mod);
-  return result;
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const vscode = __importStar(require("vscode"));
 function activate(context) {
-  const provider = vscode.languages.registerCompletionItemProvider("jsonct", {
-    provideCompletionItems(_document, _position, _token, _context) {
-      // Sample completions
-      const completions = [
-        new vscode.CompletionItem("true", vscode.CompletionItemKind.Keyword),
-        new vscode.CompletionItem("false", vscode.CompletionItemKind.Keyword),
-        new vscode.CompletionItem("null", vscode.CompletionItemKind.Keyword),
-      ];
-      return completions;
-    },
-  }, '"' // Trigger IntelliSense inside strings
-  );
-  context.subscriptions.push(provider);
+    const provider = vscode.languages.registerCompletionItemProvider("jsonct", {
+        provideCompletionItems(_document, _position, _token, _context) {
+            // Sample completions
+            const completions = [
+                new vscode.CompletionItem("true", vscode.CompletionItemKind.Keyword),
+                new vscode.CompletionItem("false", vscode.CompletionItemKind.Keyword),
+                new vscode.CompletionItem("null", vscode.CompletionItemKind.Keyword),
+            ];
+            return completions;
+        },
+    }, '"');
+    context.subscriptions.push(provider);
 }
 exports.activate = activate;
-function deactivate() {}
+function deactivate() { }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- code-extension/out/test/extension.test.js
+++ code-extension/out/test/extension.test.js
@@ -1,46 +1,26 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) ||
-  (Object.create
-    ? (function (o, m, k, k2) {
-      if (k2 === undefined) k2 = k;
-      var desc = Object.getOwnPropertyDescriptor(m, k);
-      if (
-        !desc ||
-        ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)
-      ) {
-        desc = {
-          enumerable: true,
-          get: function () {
-            return m[k];
-          },
-        };
-      }
-      Object.defineProperty(o, k2, desc);
-    })
-    : (function (o, m, k, k2) {
-      if (k2 === undefined) k2 = k;
-      o[k2] = m[k];
-    }));
-var __setModuleDefault = (this && this.__setModuleDefault) ||
-  (Object.create
-    ? (function (o, v) {
-      Object.defineProperty(o, "default", { enumerable: true, value: v });
-    })
-    : function (o, v) {
-      o["default"] = v;
-    });
-var __importStar = (this && this.__importStar) || function (mod) {
-  if (mod && mod.__esModule) return mod;
-  var result = {};
-  if (mod != null) {
-    for (var k in mod) {
-      if (
-        k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)
-      ) __createBinding(result, mod, k);
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
     }
-  }
-  __setModuleDefault(result, mod);
-  return result;
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const assert = __importStar(require("assert"));
@@ -49,10 +29,10 @@ const assert = __importStar(require("assert"));
 const vscode = __importStar(require("vscode"));
 // import * as myExtension from '../../extension';
 suite("Extension Test Suite", () => {
-  vscode.window.showInformationMessage("Start all tests.");
-  test("Sample test", () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-  });
+    vscode.window.showInformationMessage("Start all tests.");
+    test("Sample test", () => {
+        assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+        assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+    });
 });
 //# sourceMappingURL=extension.test.js.map
